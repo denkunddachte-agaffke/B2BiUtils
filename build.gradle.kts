87415e7fe39245baf71f9f7dff4f470889981ceb
Andreas Gaffke <extern.gaffke_andreas@allianz.de>
2023-08-29 15:36:35 +0200

    fix NP exception in filtering when property value is null; add filter on node values.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 
 scmVersion {
     localOnly = false
-    versionCreator("versionWithBranch")
     tag {
         prefix.set("v")
-        branchPrefix.set(mapOf("(?i)(az|allianz).*" to "az"))
+        branchPrefix.set(mapOf("(?i)(az|allianz|master|HCE).*" to "az"))
     }
+    versionCreator({versionFromTag,scmPosition -> "${versionFromTag}-az"})
 
 }
 project.version  = scmVersion.version
