bdcd22b43bd9d8374e3079aa67aa33a308129a46
Andreas Gaffke <extern.gaffke_andreas@allianz.de>
2024-05-15 16:01:44 +0200

    Add JavaTask helper to manage sources stored in custom properties.

--- B2BApiClient/src/main/java/de/denkunddachte/sfgapi/AbstractSfgKey.java
+++ B2BApiClient/src/main/java/de/denkunddachte/sfgapi/AbstractSfgKey.java
@@ -26,12 +26,12 @@ import de.denkunddachte.ft.SshKey;
 
 public abstract class AbstractSfgKey extends ApiClient {
   protected static final String ID_PROPERTY = "keyName";
-  private String                keyFingerPrint;
-  private String                keyId;
-  private int                   keyLength;
+  protected String              keyFingerPrint;
+  protected String              keyId;
+  protected int                 keyLength;
   protected String              keyName;
   protected boolean             keyStatusEnabled;
-  private SshKey                sshKey;
+  protected SshKey              sshKey;
 
   protected AbstractSfgKey() {
     super();
@@ -122,8 +122,8 @@ public abstract class AbstractSfgKey extends ApiClient {
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = super.hashCode();
+    final int prime  = 31;
+    int       result = super.hashCode();
     result = prime * result + ((sshKey == null) ? 0 : sshKey.hashCode());
     return result;
   }
